c9024ad59d82bc01df099336d86000ed0138b0b9
AleksandrBakin <aleksandr.bakin.main@gmail.com>
2022-11-18 22:51:47 +0200

    V 1.3 - Input check

--- src/main/java/mobitech/aleksbakin/test_assignment/controller/RegistrationController.java
+++ src/main/java/mobitech/aleksbakin/test_assignment/controller/RegistrationController.java
@@ -23,42 +23,52 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model){
-//        User userFromDb = userRepo.findByUsername(user.getUsername());
+        // check if user with this email already exists
         User userFromDb = userRepo.findByEmail(user.getEmail());
         if(userFromDb != null) {
             model.put("message", "User already exists!");
             return "registration";
         }
-//        String loginPattern1 = "^([a-zA-Z0-9][a-zA-Z0-9-_]?){7,19}[a-zA-Z0-9]$";
-//        String loginPattern2 = "^.{8,20}$";
-//        if(!user.getUsername().matches(loginPattern1)|| !user.getUsername().matches(loginPattern2)) {
-//            model.put("message", "Username must contain 8-20 symbols. " +
-//                    "You may use low and UPPER case English letters and numbers. " +
-//                    "You may also divide word using '_' or '-' symbols. " +
-//                    "Try one more time.");
-//            return "registration";
-//        }
-//        String passPattern1 = ".*[!-/:-@^-`{-~]+.*";
-//        String passPattern2 = ".*[a-z]+.*";
-//        String passPattern3 = ".*[A-Z]+.*";
-//        String passPattern4 = ".*[0-9]+.*";
-//        String passPattern5 = "^.{8,30}$";
-//        if(!user.getPassword().matches(passPattern1) || !user.getPassword().matches(passPattern2) ||
-//                !user.getPassword().matches(passPattern3) || !user.getPassword().matches(passPattern4) ||
-//                !user.getPassword().matches(passPattern5)) {
-//            model.put("message", "Password must contain 8-30 symbols. " +
-//                    "You need to use at lest one low and one UPPER case English letter. " +
-//                    "You need to use at lest one number and one special symbol from list: '!\"#$%&'()*+,-./:;<=>?@^_`{|}~'. " +
-//                    "Try one more time.");
-//
-//            return "registration";
-//        }
+
+        // check if e-mail correct
+        String emailPattern1 = "^[a-zA-Z0-9][a-zA-Z0-9_.-]*@[a-zA-Z0-9._-]*[.][a-zA-Z]*$";
+        if(!user.getEmail().matches(emailPattern1)) {
+            model.put("message", "Please enter real e-mail address.");
+            return "registration";
+        }
+
+        // check if age correct
+        int userAge = user.getAge();
+        if(userAge < 0 || userAge > 116) {
+            model.put("message", "Please enter real your real age.");
+            return "registration";
+        }
+        if(userAge < 14) {
+            model.put("message", "Sorry you can't sign up to our website if you are less then 14 years old.");
+            return "registration";
+        }
+
+        // check if password is difficult enough
+        String passPattern1 = ".*[!-/:-@^-`{-~]+.*";
+        String passPattern2 = ".*[a-z]+.*";
+        String passPattern3 = ".*[A-Z]+.*";
+        String passPattern4 = ".*[0-9]+.*";
+        String passPattern5 = "^.{8,30}$";
+        if(!user.getPassword().matches(passPattern1) || !user.getPassword().matches(passPattern2) ||
+                !user.getPassword().matches(passPattern3) || !user.getPassword().matches(passPattern4) ||
+                !user.getPassword().matches(passPattern5)) {
+            model.put("message", "Password must contain 8-30 symbols. " +
+                    "You need to use at lest one low and one UPPER case English letter. " +
+                    "You need to use at lest one number and one of these symbols: " +
+                    "'!\"#$%&'()*+,-./:;<=>?@^_`{|}~'.");
+
+            return "registration";
+        }
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         userRepo.save(user);
 
-        //return "redirect:/main";
         return "redirect:/main?name="+user.getName();
     }
 }
